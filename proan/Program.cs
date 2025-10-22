d12dae312b3483d0f864c718d601df33737db2cf
AxelRuiz-inbest <axel.ruiz@inbest.cloud>
2025-10-22 10:48:43 -0600

    Cambio- Reinicio de servico

--- proan/Program.cs
+++ proan/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Esto es una prueba demo del dia 22/10/2025");
+app.MapGet("/", () => "Validacion de IIS, Reinicio de servicio 22/10/2025");
 
 app.Run();
